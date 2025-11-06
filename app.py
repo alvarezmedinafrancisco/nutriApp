ec94f2e3f910d48ff5e1a20852133c1c907a9b5d
alvarezmedinafrancisco <23308060610104@cetis61.edu.mx>
2025-11-06 09:01:29 -0700

    aaaa

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask,render_template
 app = Flask(__name__)
 
-@app.route('/base')
+@app.route('/')
 def index():
     return render_template('base.html')
+
+@APP.ROUTE("/base")
+def base():
+    return render_template("base.html")
+
 
 @app.route('/info')
 def info():
